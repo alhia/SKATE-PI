1ef5e61a4c5ac118ef836fc68606581f60320aff
Alexander Hiamchan <alexander.hiamchan@appliedtechnology.se>
2019-04-11 20:00:12 +0200

    push to new repo

--- skatepi/src/Map.js
+++ skatepi/src/Map.js
@@ -60,7 +60,7 @@ class Map extends Component {
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key:  }}
+          bootstrapURLKeys={{ key: }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
